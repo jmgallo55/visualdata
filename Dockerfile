33f31f39fe61aa1499350beb9e7e8daa2921f9b4
Joe Gallo <jmgallo55@gmail.com>
2018-09-04 20:54:01 -0700

    Remove /venv

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,21 @@
-FROM python:3.6-alpine
+FROM python:3.6-alpine3.7
 
 RUN adduser -D visualdata
 
-RUN pip install --upgrade pip
+
+#for Plotly
+#RUN echo "http://dl-8.alpinelinux.org/alpine/edge/community" >> /etc/apk/repositories
+#RUN apk --no-cache --update-cache add gcc gfortran python python-dev py-pip build-base wget freetype-dev libpng-dev openblas-dev
+#RUN ln -s /usr/include/locale.h /usr/include/xlocale.h
+#RUN apk --no-cache add --virtual .builddeps gcc gfortran musl-dev     && pip install numpy==1.14.0 && pip install pandas && pip install plotly  && apk del .builddeps     && rm -rf /root/.cache
 
 WORKDIR /home/visualdata
 
 COPY requirements.txt requirements.txt
-RUN python -m venv venv
-RUN venv/bin/pip install -r requirements.txt
-RUN venv/bin/pip install gunicorn
+#RUN python -m venv venv
+RUN pip install --upgrade pip
+RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install gunicorn
 #RUN venv/bin/pip install install python3-pymysql
 
 COPY app app
@@ -19,8 +25,8 @@ RUN chmod a+x boot.sh
 
 ENV FLASK_APP visualdata.py
 
-RUN chown -R visualdata:visualdata ./
-USER visualdata
+#RUN chown -R visualdata:visualdata ./
+#USER visualdata
 
 EXPOSE 8000
 ENTRYPOINT ["./boot.sh"]
--- app/main/routes.py
+++ app/main/routes.py
@@ -22,31 +22,13 @@ from app.main import bp
 
 @bp.route('/', methods=['GET', 'POST'])
 @bp.route('/index', methods=['GET', 'POST'])
-# @login_required
 def index():
- #   form = PostForm()
- #   if form.validate_on_submit():
- #       language = guess_language(form.post.data)
- #       if language == 'UNKNOWN' or len(language) > 5:
- #           language = ''
- #       post = Post(body=form.post.data, author=current_user,
- #                   language=language)
- #       db.session.add(post)
- #       db.session.commit()
- #       flash(_('Your post is now live!'))
- #       return redirect(url_for('main.index'))
- #   page = request.args.get('page', 1, type=int)
- #   posts = current_user.followed_posts().paginate(
- #       page, current_app.config['POSTS_PER_PAGE'], False)
- #   next_url = url_for('main.index', page=posts.next_num) \
- #       if posts.has_next else None
- #   prev_url = url_for('main.index', page=posts.prev_num) \
- #       if posts.has_prev else None
     return render_template('index.html', title=_('Home'))
- #   return render_template('index.html', title=_('Home'), form=form,
- #                          posts=posts.items, next_url=next_url,
- #                          prev_url=prev_url)
 
+@bp.route('/', methods=['GET', 'POST'])
+@bp.route('/project1', methods=['GET', 'POST'])
+def project1():
+     return render_template('proj1.html', title=_('National Student Loans'))
 
 @bp.route('/explore')
 @login_required
